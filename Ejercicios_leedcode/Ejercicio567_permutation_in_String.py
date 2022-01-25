58c1d89be25488d7f0b3863f4b6af797784507e3
ming8123 <ming@hotmail.es>
2022-01-25 11:52:11 +0100

    Update Ejercicio567_permutation_in_String.py
    cambio nuevo de ejercicio 567

--- Ejercicios_leedcode/Ejercicio567_permutation_in_String.py
+++ Ejercicios_leedcode/Ejercicio567_permutation_in_String.py
@@ -13,13 +13,13 @@ def checkInclusion(self, s1: str, s2: str) -> bool:
         #primero intentamos poner una condicion para sacar el resultado 
         if s2[end] in lookup:#mira si el elemento  si esta en lookup 
             lookup[s2[end]] -= 1 #si lo esta lo quitas uno 
-            if lookup[s2[end]] >= 0:
+            if lookup[s2[end]] == 0:
                 cont += 1 #sumamos el contador para condicion del return
-        if cont == len(s1):#esta es la condicion del resultado
+        if cont == len(lookup):#esta es la condicion del resultado aqui hay que tener cuidado lo que estas comparando es len de hash map y no de s2
             return True
         if end - start >= len(s1)-1:#"digamos esto es la excepcion del resultado"
             if s2[start] in lookup: #quitamos si start se mueve a una posicion que tenga elem que esta s1                   
-                if lookup[s2[start]] >= 0:
+                if lookup[s2[start]] == 0:
                     cont -= 1
                 lookup[s2[start]]+= 1    
             start += 1              
